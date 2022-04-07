443824b685d22dce8f574762e7784f4a42c14a2e
Andrew Walters <andrewjpwalters@gmail.com>
2022-04-06 21:40:16 -0500

    With all basic deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildListItem(e.target.newTaskDescription.value)
+  })
 });
+
+
+function buildListItem(item) {
+  let li = document.createElement('li')
+  li.textContent = item
+  document.querySelector('#tasks').appendChild(li)
+}
